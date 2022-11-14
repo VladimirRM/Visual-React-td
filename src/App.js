49b6dfe29fed77439eed3d5c1cc1f09264c69411
Vladimir RM <shvdrummer@gmail.com>
2022-11-14 11:02:12 +0200

    add input and button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+<div className="wrapper">
+  <input type="text" />
+  <button></button>
+</div>
     </div>
   );
 }

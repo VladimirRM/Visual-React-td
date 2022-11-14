baf60f3e9cb2a9f173418efd68a1575e1e6e34bc
Vladimir RM <shvdrummer@gmail.com>
2022-11-14 12:10:40 +0200

    setItem(e.target.value  onchange

--- src/App.js
+++ src/App.js
@@ -3,17 +3,21 @@ import "./App.css";
 
 function App() {
   const [item, setItem] = useState("");
-  const [items, setItems] = useState (
-    JSON.parse(localStorage.getItem('items')) || []
+  const [items, setItems] = useState(
+    JSON.parse(localStorage.getItem("items")) || []
   );
 
-  useEffect(()=>{
-   localStorage.setItem('items', JSON.stringify(items))
-  },[items])
+  useEffect(() => {
+    localStorage.setItem("items", JSON.stringify(items));
+  }, [items]);
   return (
     <div className="App">
       <div className="wrapper">
-        <input type="text" placeholder="Enter something" />
+        <input
+          type="text"
+          placeholder="Enter something"
+          onChange={(e) => setItem(e.target.value)}
+        />
         <button className="enter">Enter</button>
       </div>
     </div>
